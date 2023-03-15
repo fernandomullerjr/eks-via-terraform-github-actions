1a46351108f2c90a33c6554abd0fcea51f50999e
Fernando Muller Junior <fernandomj90@gmail.com>
2023-03-14 22:20:22 -0300

    Projeto - eks-via-terraform-github-actions

---
+++ 07-eks-lab/data.tf
@@ -0,0 +1,11 @@
+
+# Find the user currently in use by AWS
+data "aws_caller_identity" "current" {}
+
+# Region in which to deploy the solution
+data "aws_region" "current" {}
+
+# Availability zones to use in our solution
+data "aws_availability_zones" "available" {
+  state = "available"
+}
